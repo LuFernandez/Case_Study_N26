9bf3124ad405b46e4535325a00a3a631317e861a
Lu <LuFernandez@users.noreply.github.com>
2024-07-31 01:29:00 -0300

    wip error handling

--- force-app/main/default/lwc/contactProducts/contactProducts.js
+++ force-app/main/default/lwc/contactProducts/contactProducts.js
@@ -1,8 +1,40 @@
-import { LightningElement, wire } from 'lwc';
-import getData from '@salesforce/apex/ContactController.getContactProductInfo';
-
+import { LightningElement, api, wire } from 'lwc';
+import getContactInfo from '@salesforce/apex/ContactController.getCaseRelatedContact';
+import getProductInfo from '@salesforce/apex/ProductController.getProductInfo';
 
 export default class ContactProducts extends LightningElement {
-    @wire(getData) data;
-}
+    @api recordId;
+    caseInfo;
+    productData;
+    prodMap;
+    error;
 
+    @wire(getContactInfo, { caseId: '$recordId' })
+    wiredCase({ error, data }) {
+        if (data) {
+            if(!data.hasOwnProperty('ContactId')){
+                this.caseInfo = undefined;
+                this.error = true;
+            }else{
+                this.caseInfo = data;
+            }
+        } else if (error) {
+            this.error = error;
+            this.caseInfo = undefined;
+        }
+        console.log(this.caseInfo);
+    }
+
+    @wire(getProductInfo, {productId: '$caseInfo.Contact.Product__c', country: '$caseInfo.Contact.Home_Country__c'})
+    wiredProductData({ error, data }) {
+        if(data){
+            console.log(data);
+            console.log(data.prodName);
+            this.productData = data;
+            // this.productData.atmFee = data.atmFee/100;
+            if (error){
+                console.log(error);
+            }
+        }
+    }
+}
